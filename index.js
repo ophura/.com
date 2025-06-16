bc180ec1a01a16f6f222b97518cc8eba2ae94475
8chan-co <8chan.com@gmail.com>
2025-06-16 02:30:51 +0200

    dynamic font resizing

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log(1, 2, 3);
+const ThisWebsite = `https://ophura.com`;
+const VRChatAccount = `https://vrchat.com/home/user/usr_2b4e288f-a8cb-48bc-afe9-72be97e83842`;
+const GitHubAccount = `https://github.com/8chan-co`;
+
+console.log(ThisWebsite, VRChatAccount, GitHubAccount);
