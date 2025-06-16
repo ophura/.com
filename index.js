ec623c82cece3b645a00e06d975a83bed00c218e
8chan-co <8chan.com@gmail.com>
2025-06-16 03:09:16 +0200

    yuris

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const ThisWebsite = `https://ophura.com`;
-const VRChatAccount = `https://vrchat.com/home/user/usr_2b4e288f-a8cb-48bc-afe9-72be97e83842`;
-const GitHubAccount = `https://github.com/8chan-co`;
+const ThisWebsite = new URL('https://ophura.com');
+const VRChatAccount = new URL('https://vrchat.com/home/user/usr_2b4e288f-a8cb-48bc-afe9-72be97e83842');
+const GitHubAccount = new URL('https://github.com/8chan-co');
 
-console.log(ThisWebsite, VRChatAccount, GitHubAccount);
+console.group(`related links`);
+console.log([ThisWebsite.href, VRChatAccount.href, GitHubAccount.href].join('\u000a'));
+console.groupEnd();
