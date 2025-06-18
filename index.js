2a5212fb525b39d094ec38e500fd8b398f27935e
Ophura <8chan.com@gmail.com>
2025-06-18 15:10:59 +0200

    space y

--- index.js
+++ index.js
@@ -22,9 +22,13 @@ document['addEventListener']('DOMContentLoaded', () => {
 });
 
 const ThisWebsite = new this['URL']('https://ophura.com');
+
 const VRChatAccount = new this['URL']('https://vrchat.com/home/user/usr_2b4e288f-a8cb-48bc-afe9-72be97e83842');
+
 const GitHubAccount = new this['URL']('https://github.com/8chan-co/ophura.com');
 
 console['group']('related links');
+
 console['log']([ThisWebsite['href'], VRChatAccount['href'], GitHubAccount['href']]['join']('\n'));
+
 console['groupEnd']();
