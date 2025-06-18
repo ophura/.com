68ae884370abc9ea997a18e57c1fc0c96a033455
8chan-co <8chan.com@gmail.com>
2025-06-18 14:54:28 +0200

    scriptup

--- index.js
+++ index.js
@@ -20,9 +20,11 @@ const SetYear = (Element) => {
     Element['appendChild'](document['createTextNode'](Year));
 }
 
-const ThisWebsite = new URL('https://ophura.com');
-const VRChatAccount = new URL('https://vrchat.com/home/user/usr_2b4e288f-a8cb-48bc-afe9-72be97e83842');
-const GitHubAccount = new URL('https://github.com/8chan-co/ophura.com');
+document['addEventListener']('DOMContentLoaded', () => SetYear(document['getElementById']('Year')));
+
+const ThisWebsite = new this['URL']('https://ophura.com');
+const VRChatAccount = new this['URL']('https://vrchat.com/home/user/usr_2b4e288f-a8cb-48bc-afe9-72be97e83842');
+const GitHubAccount = new this['URL']('https://github.com/8chan-co/ophura.com');
 
 console['group']('related links');
 console['log']([ThisWebsite['href'], VRChatAccount['href'], GitHubAccount['href']]['join']('\n'));
