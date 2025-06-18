86d9fcc18f824a1d2777f93aa61e08073b9324c8
8chan-co <8chan.com@gmail.com>
2025-06-18 13:20:19 +0200

    proper repo

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const ThisWebsite = new URL('https://ophura.com');
 const VRChatAccount = new URL('https://vrchat.com/home/user/usr_2b4e288f-a8cb-48bc-afe9-72be97e83842');
-const GitHubAccount = new URL('https://github.com/8chan-co');
+const GitHubAccount = new URL('https://github.com/8chan-co/ophura.com');
 
 console.group('related links');
 console.log([ThisWebsite.href, VRChatAccount.href, GitHubAccount.href].join('\n'));
