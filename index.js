501553905a455eb3d9565e6884e776d3dbedecc1
8chan-co <8chan.com@gmail.com>
2025-07-31 23:19:38 +0200

    datie

--- index.js
+++ index.js
@@ -1,33 +1,20 @@
 'use strict';
 
-document['addEventListener']('DOMContentLoaded', () => {
+const YearOnly = Intl.DateTimeFormat(undefined, { year: 'numeric' });
 
-    const Element = document['getElementById']('year');
+document.addEventListener('DOMContentLoaded', () => {
 
-    if (!Element) return;
+    year.removeChild(year.firstChild);
 
-    if (Element['hasChildNodes']()) {
-
-        for (const Node of Element['childNodes']) {
-
-            if (Node['nodeType'] === Node['TEXT_NODE']) {
-
-                Element['removeChild'](Node);
-            }
-        }
-    }
-
-    const Year = new this['Date']()['getFullYear']()['toString']();
-
-    Element['appendChild'](document['createTextNode'](Year));
+    year.appendChild(document.createTextNode(YearOnly.format(Date.now())));
 });
 
-const VRChatAccount = new this['URL']('https://vrchat.com/home/user/usr_2b4e288f-a8cb-48bc-afe9-72be97e83842');
+const VRChatAccount = 'https://vrchat.com/home/user/usr_2b4e288f-a8cb-48bc-afe9-72be97e83842';
 
-const GitHubAccount = new this['URL']('https://github.com/8chan-co/ophura.com');
+const GitHubAccount = 'https://github.com/8chan-co/ophura.com';
 
-console['group']('related links');
+console.group('related links');
 
-console['log']([location['href'], VRChatAccount['href'], GitHubAccount['href']]['join']('\n'));
+console.log([location.href, VRChatAccount.href, GitHubAccount.href].join('\n'));
 
-console['groupEnd']();
+console.groupEnd();
