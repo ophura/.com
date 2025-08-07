57c244dc591b0f45b984f6e4d78c0460491ee980
8chan-co <8chan.com@gmail.com>
2025-08-07 19:10:18 +0200

    mdrn

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ const YearOnly = Intl.DateTimeFormat(undefined, { year: 'numeric' });
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    year.removeChild(year.firstChild);
-
-    year.appendChild(document.createTextNode(YearOnly.format(Date.now())));
+    year.replaceChild(year.firstChild, document.createTextNode(YearOnly.format(Date.now())));
+    
 });
 
 const VRChatAccount = 'https://vrchat.com/home/user/usr_2b4e288f-a8cb-48bc-afe9-72be97e83842';
